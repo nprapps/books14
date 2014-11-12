16140809baef6c8a5001e79e9802fc3bf1cb49f8
David Eads <davideads@gmail.com>
2014-11-12 13:16:54 -0500

    use route from old app

--- app.py
+++ app.py
@@ -14,18 +14,51 @@ app = Flask(__name__)
 app.add_template_filter(smarty_filter, name='smarty')
 app.add_template_filter(urlencode_filter, name='urlencode')
 
-# Example application views
 @app.route('/')
 def index():
     """
-    Example view demonstrating rendering a simple HTML page.
+    The index page.
     """
+    # Set up standard page context.
     context = make_context()
 
-    with open('data/featured.json') as f:
-        context['featured'] = json.load(f)
+    # Read the books JSON into the page.
+    with open('www/static-data/books.json', 'rb') as readfile:
+        context['books_js'] = readfile.read()
+        books = json.loads(context['books_js'])
+        books_text_only = books[:]
+        books_text_only = sorted(books, key=lambda k: k['title'])
 
-    return make_response(render_template('index.html', **context))
+    tag_stripper = re.compile(r'<.*?>')
+
+    for book in books:
+        if not book['text']:
+            book['teaser'] = None
+            continue
+
+        img = Image.open('www/assets/cover/%s-thumb.jpg' % book['slug'])
+        width, height = img.size
+
+        # Poor man's packing algorithm. How much text will fit?
+        chars = height / 25 * 10;
+
+        text = tag_stripper.sub('', book['text'])
+
+        if len(text) <= chars:
+            book['teaser'] = text
+            continue
+
+        i = chars
+
+        while text[i] != ' ':
+            i -= 1
+
+        book['teaser'] = '&#8220;' + text[:i] + ' ...&#8221;'
+
+    context['books'] = books
+    context['books_text_only'] = books_text_only
+
+    return render_template('index.html', **context)
 
 @app.route('/comments/')
 def comments():
